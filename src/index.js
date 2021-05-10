35655ef3def45d919db6267fda37558c8b15338b
ckh22 <45274762+ckh22@users.noreply.github.com>
2021-05-10 16:36:50 -0700

    BIP

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import App from './Layout/App';
 
 function render() {
-  ReactDOM.render (<div>Build In Progress!</div>, document.getElementById('root'));
+  ReactDOM.render (<div style={{display: 'flex', justifyContent: 'center', alignItems: 'center', fontSize: '32px'}}>Build In Progress!</div>, document.getElementById('root'));
 }
 
 if (module.hot) {
