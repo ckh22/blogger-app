bef4fdb5e2c822bdf557fd143f171ca68fe7fc25
ckh22 <45274762+ckh22@users.noreply.github.com>
2021-05-10 16:39:42 -0700

    BUG

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './Layout/App';
+//import App from './Layout/App';
 
 function render() {
   ReactDOM.render (<div style={{display: 'flex', justifyContent: 'center', alignItems: 'center', fontSize: '32px'}}>Build In Progress!</div>, document.getElementById('root'));
