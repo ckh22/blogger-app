8031c0d29e1cdc3fecb9c4c91b8d14ade703f921
ckh22 <45274762+ckh22@users.noreply.github.com>
2021-05-11 17:20:00 -0700

    BIP

--- src/index.js
+++ src/index.js
@@ -2,17 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter} from 'react-router-dom';
 import './index.scss'
-import App from './Layout/App';
+// import App from './Layout/App';
 import {Provider} from "react-redux";
 import {configureStore} from "./Store/configureStore.js";
 
-// const App = <div style={{height: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center', fontSize: '32px'}}>Build In Progress!</div>
+const App = <div style={{height: '100vh', display: 'flex', justifyContent: 'center', alignItems: 'center', fontSize: '32px'}}>Build In Progress!</div>
 
 const store = configureStore();
 
 function render() {
     ReactDOM.render (<Provider store={store}>
-        <BrowserRouter><App/></BrowserRouter>
+        <BrowserRouter>{App}</BrowserRouter>
     </Provider>, document.getElementById('root'));
 }
 
